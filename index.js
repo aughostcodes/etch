496d6c34778e42d9cc34c116cc4c8306105318a7
August <hello@aughost.codes>
2020-03-03 17:39:35 -0500

    Functioning as intended

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ function appendDivsToGrid(num) {
         createdDiv.classList.add('box');
         gridContainer.appendChild(createdDiv);
     }
-};
+}
 
 function setGrid(size) {
     gridContainer.setAttribute('style',
         `grid-template-columns: repeat(${size}, 1fr);
     grid-template-rows: repeat(${size}, 1fr);`);
-};
+}
 
 function removeGridKids() {
     while (gridContainer.firstChild) {
@@ -45,23 +45,17 @@ function generateRandomColor() {
     return randomColor;
 }
 
-// don't need to add another event listener inside this one, it can just do stuff itself
 function drawOnGrid(event) {
     console.log('drawing on grid', event);
 
-    // mouseover fires on the element (grid) itself and all child elements being moused over
-    // in this case we only care when a child element ('.box') is moused over
-    // so we do this extra check here
     if (event.target.classList.contains('box')) {
         event.target.classList.add('hovered');
     }
-};
+}
 
-// don't need to add another event listener inside this one, it can just do stuff itself
 function clickOnGrid(event) {
     console.log('clicking on grid', event);
 
-    // this check is not technically needed for click in this case, but feels safer to me
     if (event.target.classList.contains('box')) {
         event.target.classList.toggle('clicked');
     }
@@ -78,7 +72,6 @@ function colorOnGrid(event) {
 const drawMode = function () {
     console.log('setting draw mode');
 
-    // notice I'm adding/removing the event listeners directly on the grid
     gridContainer.addEventListener('mouseover', drawOnGrid);
     gridContainer.removeEventListener('click', clickOnGrid);
     gridContainer.removeEventListener('mouseover', colorOnGrid);
@@ -87,7 +80,6 @@ const drawMode = function () {
 const pixelMode = function () {
     console.log('setting pixel mode');
 
-    // notice I'm adding/removing the event listeners directly on the grid
     gridContainer.addEventListener('click', clickOnGrid);
     gridContainer.removeEventListener('mouseover', drawOnGrid);
     gridContainer.removeEventListener('mouseover', colorOnGrid);
@@ -101,9 +93,6 @@ const colorMode = function () {
     gridContainer.removeEventListener('mouseover', clickOnGrid);
 }
 
-// set click handler to be a function, not the result of a function -- no () after
-// this was causing trouble previously because it would execute drawMode() and pixelMode() immediately
-// could also use .addEventListener('click', function) here instead
 drawModeButton.addEventListener('click', drawMode);
 pixelModeButton.addEventListener('click', pixelMode);
 colorModeButton.addEventListener('click', colorMode);
@@ -117,22 +106,20 @@ clearButton.addEventListener('click', function () {
         appendDivsToGrid(newDivNumber);
         setGrid(newDivNumber);
     }
-})
+});
 
 defaultButton.addEventListener('click', function () {
     removeGridKids();
     appendDivsToGrid(defaultDivNumber);
     setGrid(defaultDivNumber);
-})
+});
 
 changeSizeButton.addEventListener('click', function () {
     newDivNumber = parseInt(prompt('Enter a number of squares.'));
     removeGridKids();
     appendDivsToGrid(newDivNumber);
     setGrid(newDivNumber);
-})
-
-
+});
 
 appendDivsToGrid(defaultDivNumber);
 setGrid(defaultDivNumber);
